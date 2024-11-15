ee76f2291b6f06416db578e52b1db216b3f9d70e
pablo unzueta <punzu001@ucr.edu>
2024-11-15 15:18:34 -0800

    change * to @

--- train_gpt2.py
+++ train_gpt2.py
@@ -121,7 +121,7 @@ class CausalSelfAttention(nn.Module):
                 is_causal=True,
             )
         else:  # manual implementation of attention
-            att = (q * k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
             att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
             att = F.softmax(att, dim=-1)
             att = self.attn_dropout(att)
