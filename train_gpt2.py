80c6f1d47ca8cc2b6c60bad67cce6712f28d6f44
pablo unzueta <punzu001@ucr.edu>
2024-11-15 14:09:48 -0800

    fixed typo in transformer def

--- train_gpt2.py
+++ train_gpt2.py
@@ -142,7 +142,7 @@ class GPT(nn.Module):
                 wte=nn.Embedding(config.vocab_size, config.n_embd),
                 wpe=nn.Embedding(config.sequence_length, config.n_embd),
                 drop=nn.Dropout(config.dropout),
-                h=nn.ModuleList([Block(config) for _ in range(config.n_layers)]),
+                h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
                 ln_f=LayerNorm(config.n_embd, bias=config.bias),
             )
         )
